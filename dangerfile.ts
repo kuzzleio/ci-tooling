f436b155ac4928a3c7c4312fc0650221000ab514
Shiranuit <ycombes@kuzzle.io>
2022-06-09 11:41:29 +0200

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,11 +5,13 @@ import * as fs from 'fs';
 async function main() {
   const tsFiles = danger.git.created_files.filter(e => e.endsWith('.ts'));
 
-  for (const file in tsFiles) {
+  for (const file of tsFiles) {
     console.log(`./${file}`);
+    const jsonDiff = await danger.git.structuredDiffForFile(file);
     message(`./${file}`);
     const content = fs.readFileSync(`./${file}`).toString();
     message(content);
+    message(JSON.stringify(jsonDiff, null, 2));
   }
 }
 
