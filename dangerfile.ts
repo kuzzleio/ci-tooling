2538c17ae7866ff31903c89fc6291a46c9210cec
Shiranuit <ycombes@kuzzle.io>
2022-06-09 10:58:27 +0200

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,4 +2,4 @@ import {danger, warn, message} from 'danger';
 
   
 // No PR is too small to include a description of why you made a change
-message(danger.git.modified_files.join('\n'));
+message(danger.git.created_files.join('\n'));
