0c5490832d6f4e92b5b1dddaec5b355e6fec29b0
Shiranuit <ycombes@kuzzle.io>
2022-06-09 11:33:17 +0200

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -6,6 +6,7 @@ async function main() {
   const tsFiles = danger.git.created_files.filter(e => e.endsWith('.ts'));
 
   for (const file in tsFiles) {
+    message(`./${file}`);
     const content = fs.readFileSync(`./${file}`).toString();
     message(content);
   }
