25e76181900052dbd3f39812a4518b5d8186408e
Shiranuit <ycombes@kuzzle.io>
2022-06-09 10:51:29 +0200

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,5 @@
-import {danger, warn} from 'danger';
+import {danger, warn, message} from 'danger';
 
   
 // No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
-}
-
-  
+message(danger.github.pr.body);
