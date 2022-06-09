da4a57d6960d1aa25170911142ecaef9dbe2aa31
Shiranuit <ycombes@kuzzle.io>
2022-06-09 11:13:03 +0200

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,13 +5,13 @@ async function main() {
   const tsFiles = danger.git.created_files.filter(e => e.endsWith('.ts'));
 
   for (let i = 0; i < 10; i++) {
-    message(''+i);
+    await message(''+i);
   }
 
   for (const file in tsFiles) {
 
     const jsonDiff = await danger.git.JSONDiffForFile(file);
-    message(JSON.stringify(jsonDiff, null, 2));
+    await message(JSON.stringify(jsonDiff, null, 2));
   }
 }
 
