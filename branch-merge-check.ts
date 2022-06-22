e7dc0bc493f3205339fc2e14bc5fa2137b4901f4
Shiranuit <shiranui007@gmail.com>
2022-06-22 18:12:58 +0200

    wip

--- branch-merge-check.ts
+++ branch-merge-check.ts
@@ -4,7 +4,7 @@ import {danger, message} from 'danger';
 async function main() {
   const base = danger.github.pr.base.ref;
   const head = danger.github.pr.head.ref;
-  message(`${base} -> ${head}`);
+  message(`${base} -> ${head} [${process.env.RULES}]`);
 }
 
 main();
