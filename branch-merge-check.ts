5310e6f020fe783cd6c3f94b6fe416e88af60f7c
Shiranuit <shiranui007@gmail.com>
2022-06-22 18:39:47 +0200

    wip

--- branch-merge-check.ts
+++ branch-merge-check.ts
@@ -1,10 +1,47 @@
-import {danger, message} from 'danger';
+import {danger, fail} from 'danger';
 
- 
+function checkBypass(baseBranch: string, body: string): boolean {
+  if (! body) {
+    return false;
+  }
+
+  const allowedBranch = body.match(/ci-allow-merge-into *: *(\S+)/);
+  return allowedBranch ? allowedBranch![1] === baseBranch : false;
+}
+
+function sendFailureMessage() {
+
+}
+
 async function main() {
   const base = danger.github.pr.base.ref;
   const head = danger.github.pr.head.ref;
-  message(`${base} -> ${head} [${process.env.RULES}]`);
+  const body = danger.github.pr.body;
+
+  if (! process.env.RULES || checkBypass(base, body)) {
+    return;
+  }
+
+  const rules = JSON.parse(process.env.RULES);
+  
+  if (! rules[base]) {
+    return;
+  }
+
+  const allowedBranches = rules[base];
+
+  if ((Array.isArray(allowedBranches) && ! allowedBranches.includes(head))
+    || (typeof allowedBranches === 'string' && allowedBranches !== head)
+  ) {
+    fail(`
+    Merging "${head}" into "${base}" is not allowed.
+    If you want to allow this PR to be merged into "${base}", add the following line to your PR body:
+    \`\`\`
+    ci-allow-merge-into: ${base}
+    \`\`\`
+    `);
+    return;
+  }
 }
 
 main();
