10386efde2b159fa608370008a635bc2bf56566b
Shiranuit <shiranui007@gmail.com>
2022-06-27 13:45:42 +0200

    fix workflow

--- danger.ts
+++ danger.ts
@@ -3,6 +3,12 @@ import { schedule } from 'danger';
 
 schedule(async () => {
   console.log(__dirname);
+  console.log(process.env.REPOSITORY);
+  console.log(process.env.GITHUB_REF_NAME);
+  console.log(process.env.GITHUB_REF);
+  console.log(process.env.GITHUB_PATH);
+  console.log(process.env.GITHUB_WORKSPACE);
+  console.log(process.env.GITHUB_HEAD_REF);
+  console.log(process.env.GITHUB_BASE_REF);
   console.log(process.env.DANGER_CONFIG);
-  throw 'My Error';
 });
