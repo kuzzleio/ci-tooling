f9dcbb19e5aefd591a2e09f82c6b4470d75329b5
Shiranuit <shiranui007@gmail.com>
2022-06-28 14:36:56 +0200

    rename check

--- checks/pr-body-check/test.ts
+++ checks/pr-body-check/test.ts
@@ -1,7 +1,13 @@
 import { CheckResult, DangerCheck } from '../../lib/DangerCheck';
+import { DangerConfig } from '../../lib/types/DangerConfig';
 import { DangerUtils } from '../../lib/utils/DangerUtils';
 
 export default class PRBodyCheck extends DangerCheck {
+
+  constructor (config: DangerConfig) {
+    super(config);
+    this.name = 'PR Body Check';
+  }
 
   async run(): Promise<CheckResult> {
     if (DangerUtils.github.pr.body.length === 0) {
